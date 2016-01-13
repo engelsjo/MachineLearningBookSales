66827922b31c23201aa204a6824425e390cbdc71
Josh <engelsjo@mail.gvsu.edu>
2016-01-13 13:24:57 -0500

    initial file commits

---
+++ project1.js
@@ -0,0 +1,9 @@
+/* ***************************************************************************
+ * @Title Machine Learning Project # 1
+ * @Description: program to analyze data from book sales, and make predictions
+ * @Authors: Michael Baldwin, Josh Engelsma, Adam Terwilliger
+ * @Date: January 13, 2016
+ * @Version 1.0
+ ****************************************************************************/
+
+ var fileName = 'downloads.txt'
