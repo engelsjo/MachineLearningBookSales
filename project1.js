b52be835e83380926d5a345049419f9762468afb
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-01-13 20:34:54 -0500

    working d3 scatterplot

--- project1.js
+++ project1.js
@@ -8,29 +8,62 @@
 
 function init() {
 
-    var w = 1000;
-    var h = 1000;
+    // width and height
+    var w = 300;
+    var h = 300;
+    var padding = 50;
 
+    // scale functions
+    var xScale = d3.scale.linear()
+                     .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+                     .range([padding, w - padding]);
+
+    var yScale = d3.scale.linear()
+                    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+                    .range([h - padding, padding]);
+
+    // define X-axis
+	var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom")
+		.ticks(10);
+
+	// define Y-axis
+    var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left")
+		.ticks(5);
+
+    // create svg element
     var svg = d3.select("body")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
 
+    // create circles
     svg.selectAll("circle")
         .data(dataset)
         .enter()
         .append("circle")
         .attr("cx", function(d) {
-            return d[0];
+            return xScale(d[0]);
         })
         .attr("cy", function(d) {
-            /*
-            invert to not be graphics programming where origin
-            is at top, left, but (0,0) origin at bottom, left
-            */
-            return h - (d[1] / 10);
+            return yScale(d[1]);
         })
-        .attr("r", 5);
+        .attr("r", 1);
+
+    // create X-axis
+    svg.append("g")
+    	.attr("class", "axis")
+    	.attr("transform", "translate(0," + (h - padding) + ")")
+    	.call(xAxis);
+
+    // create Y-axis
+    svg.append("g")
+    	.attr("class", "axis")
+    	.attr("transform", "translate(" + padding + ",0)")
+    	.call(yAxis);
 }
 
 window.onload = function() {
