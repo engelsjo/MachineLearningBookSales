2e5fde2034081f3761e7357254dcbf25dca07652
Josh <engelsjo@mail.gvsu.edu>
2016-01-16 17:45:26 -0500

    code cleanup

--- bookTrendReader.py
+++ bookTrendReader.py
@@ -13,10 +13,13 @@ class TrendReader(object):
 	def __init__(self, dataFile):
 		self.dataFile = dataFile
 		self.fileLines = []
-		self.slope = 0
-		self.intercept = 0
+		self.nbrOfNans = 0
+		self.linearCoefficients = {}
+		self.cubicCoefficients = {}
 		self.predictedLinearValues = []
 		self.predictedCubicValues = []
+		self.cubicAndLinearAvgValues = []
+		self.nanPredictedValues = {}
 
 	def readDataFile(self):
 		"""
@@ -29,69 +32,66 @@ class TrendReader(object):
 				downloads = lineParts[1]
 				if downloads != "nan":
 					downloads = int(downloads)
+				else:
+					self.nbrOfNans += 1
 				self.fileLines.append(downloads)
 
 	def generateLinearRegression(self):
 		"""
 		@summary: method to generate a linear regression from our data
 		"""
-		sumXvalues = sumYvalues = sumXYpairs = sumXsquared = sumYsquared = nbrOfNans = 0
+		sumXvalues = sumYvalues = sumXYpairs = sumXsquared = sumYsquared = 0
 		for hour, downloads in enumerate(self.fileLines):
-			if downloads == "nan":
-				nbrOfNans += 1
-				continue
+			if downloads == "nan": continue
 			sumXvalues += hour
 			sumYvalues += downloads
 			sumXYpairs += (hour * downloads)
 			sumXsquared += pow(hour, 2)
 			sumYsquared += pow(downloads, 2)
-		nbrOfValues = len(self.fileLines) - nbrOfNans
+		nbrOfValues = len(self.fileLines) - self.nbrOfNans
 		# calculate the slope
 		slope = (1.0 * ((nbrOfValues * sumXYpairs) - (sumXvalues * sumYvalues))) / (1.0 * ((nbrOfValues * sumXsquared) - (sumXsquared)))
 		# calculate the y intercept
 		intercept = (1.0 * (sumYvalues - (slope * sumXvalues))) / (1.0 * nbrOfValues)
 
 		# assign to the instance
-		self.slope = slope
-		self.intercept = intercept
+		self.linearCoefficients["slope"] = slope
+		self.linearCoefficients["intercept"] = intercept
 		print("\nLinear Regression:\ny = {}x + {}\n".format(slope, intercept))
 
 	def generateCubicRegression(self):
 		"""
 		@summary: method to generate a cubic regression from our data
 		"""
-		sumX6Values = sumX5Values = sumX4Values = sumX3Values = sumX2Values = sumX1Values = sumX2Y1Values = sumX3Y1Values = sumX1Y1Values = sumY1Values = nbrOfNans = 0
+		xSummations = [0, 0, 0, 0, 0, 0]
+		xySummations = [0, 0, 0, 0]
+
 		for hour, downloads in enumerate(self.fileLines):
-			if downloads == "nan":
-				nbrOfNans += 1
-				continue
-			sumX1Values += hour
-			sumX2Values += pow(hour, 2)
-			sumX3Values += pow(hour, 3)
-			sumX4Values += pow(hour, 4)
-			sumX5Values += pow(hour, 5)
-			sumX6Values += pow(hour, 6)
-			sumX3Y1Values += pow(hour, 3) * downloads
-			sumX2Y1Values += pow(hour, 2) * downloads
-			sumX1Y1Values += hour * downloads
-			sumY1Values += downloads
-		nbrOfValues = len(self.fileLines) - nbrOfNans
+			if downloads == "nan": continue
+			for exponent in range(1, 7): xSummations[exponent - 1] += pow(hour, exponent)
+			for exponent in range(0, 4): xySummations[exponent] += pow(hour, exponent) * downloads	
+		nbrOfValues = len(self.fileLines) - self.nbrOfNans
 
-		# build matrices to calculate the 4 coefficients
-		coeffMatrix = [
-						[sumX6Values, sumX5Values, sumX4Values, sumX3Values],
-						[sumX5Values, sumX4Values, sumX3Values, sumX2Values],
-						[sumX4Values, sumX3Values, sumX2Values, sumX1Values],
-						[sumX3Values, sumX2Values, sumX1Values, nbrOfValues]
-					  ]
-		answeMatrix = [
-						[sumX3Y1Values],
-						[sumX2Y1Values],
-						[sumX1Y1Values],
-						[sumY1Values]
-					  ]
+		# build coefficient matrix
+		coeffMatrix = []
+		for row in range(4):
+			matrixRow = []
+			for col in range(4):
+				if row == 3 and col == 3: matrixRow.append(nbrOfValues)
+				else: matrixRow.append(xSummations[5 - row - col])
+			coeffMatrix.append(matrixRow)
 
+		# build the answers matrix
+		answeMatrix = []
+		for row in range(4):
+			rowMatrix = [xySummations[3 - row]]
+			answeMatrix.append(rowMatrix)
+
+		# use numpy to solve the matrix equation, and store off our data
 		resultsMatrix = np.linalg.solve(np.array(coeffMatrix), np.array(answeMatrix))
+		keys = ["cubicCoeff", "squaredCoeff", "xCoeff", "intercept"]
+		for i in range(4):
+			self.cubicCoefficients[keys[i]] = resultsMatrix[i][0]
 		print("\nCubic Regression:\ny = {}x^3 + {}x^2 + {}x + {}\n".format(resultsMatrix[0][0], resultsMatrix[1][0], resultsMatrix[2][0], resultsMatrix[3][0]))
 
 
@@ -102,6 +102,13 @@ class TrendReader(object):
 		for hourIndex in range(1, len(self.fileLines) + 1):
 			predictedValue = self.slope * hourIndex + self.intercept
 			self.predictedLinearValues.append(predictedValue)
+
+	def generateCubicPredictedValues(self):
+		"""
+		@summary: method to generate the predicted values in our cubic regression model
+		"""
+		for hourIndex in range(1, len(self.fileLines) + 1):
+			predictedValue = 0
 
 	def getLinearPredictedValues(self):
 		return self.predictedLinearValues
