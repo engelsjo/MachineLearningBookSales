ffa92e329872b10d9cab15ec9ea807dbc3a4e4a0
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-01-18 23:28:14 -0500

    barchart, simple

--- js/barchart.js
+++ js/barchart.js
@@ -9,100 +9,33 @@
  /* http://alignedleft.com/tutorials/d3/making-a-bar-chart */
  function barchart() {
 
-     // width and height
-     var w = 500;
-     var h = 100;
+    // width and height
+    var w = 500;
+    var h = 500;
+    var barHeight = 20;
 
-     // svg element
-     var svg = d3.select("body")
-                 .append("svg")
-                 .attr("width", w)
-                 .attr("height", h);
+    var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr("class", "chart");
 
-    // create rectangles
-    svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", function(d, i) {
-           return i * 21;
-       })
-       .attr("y", 0)
-       .attr("width", 20)
-       .attr("height", 100);
+    var chart = d3.select(".chart")
+    .attr("width", w)
+    .attr("height", barHeight * dataset.length);
 
-/*
-       .attr("x", function(d, i) {
-           return i * (w / dataset.length);
-       })
-       .attr("y", function(d) {
-           return h - d; // height minus data value
-       })
-       .attr("width", w / dataset.length - barPadding)
-       .attr("height", function(d) {
-           return d; // just the data value
-       });
-       */
+    var x = d3.scale.linear()
+    .domain([0, d3.max(dataset)])
+    .range([0, w]);
 
-/*
-     // width and height
-     var w = 500;
-     var h = 100;
-     var padding = 50;
+    var bar = chart.selectAll("g")
+    .data(dataset)
+    .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-     // scale functions
-     var xScale = d3.scale.linear()
-                      .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-                      .range([padding, w - padding]);
-
-     var yScale = d3.scale.linear()
-                     .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-                     .range([h - padding, padding]);
-
-     // define X-axis
- 	var xAxis = d3.svg.axis()
- 		.scale(xScale)
- 		.orient("bottom")
- 		.ticks(10);
-
- 	// define Y-axis
-     var yAxis = d3.svg.axis()
- 		.scale(yScale)
- 		.orient("left")
- 		.ticks(5);
-
-     // create svg element
-     var svg = d3.select("body")
-                 .append("svg")
-                 .attr("width", w)
-                 .attr("height", h);
-
-     // create circles
-     svg.selectAll("circle")
-         .data(dataset)
-         .enter()
-         .append("circle")
-         .attr("cx", function(d) {
-             return xScale(d[0]);
-         })
-         .attr("cy", function(d) {
-             return yScale(d[1]);
-         })
-         .attr("r", 1);
-
-     // create X-axis
-     svg.append("g")
-     	.attr("class", "axis")
-     	.attr("transform", "translate(0," + (h - padding) + ")")
-     	.call(xAxis);
-
-     // create Y-axis
-     svg.append("g")
-     	.attr("class", "axis")
-     	.attr("transform", "translate(" + padding + ",0)")
-     	.call(yAxis);
-
-*/
+    bar.append("rect")
+    .attr("width", w)
+    .attr("height", barHeight - 1);
  }
 
 function init() {
